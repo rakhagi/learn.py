acb9aa6901ca6ca1b2c123250379f693a2ebfbf4
rakhagi <rahmadkhamari@gmail.com>
2022-05-04 22:04:01 +0700

    learn.py

---
+++ myfirstcode.py
@@ -0,0 +1,6 @@
+a = input ('masukan angka: ') #input function
+
+if int(a) > 5: #if function with type:integer variable
+  print('nilai a lebih dari 5')
+else:  #else functio 
+	print('nilai kurang dari 5')
